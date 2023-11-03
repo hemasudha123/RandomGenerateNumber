7a5c6b20da283355aa095a05cee1e6e528959a58
HemaSudha <hemasudha.a@way2news.com>
2023-11-03 12:12:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react'
 function App() {
+  const [generaterandom, setGenerateRandom] = useState(0)
+
+  const handleGenerate = () => {
+    let generatebutton = Math.floor(Math.random() * 101);
+    setGenerateRandom(generatebutton)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="randomContainer">
+      <div className="randomparent">
+        <h1>Random Number</h1>
+        <p>Generate a random number in the range of 0 to 100</p>
+        <button onClick={handleGenerate}>Generate</button>
+       <span className='number'>{generaterandom}</span>
+      </div>
+
     </div>
   );
 }
